5808475c2ea4f3291c7c391e3d1b3e813441a3c2
Justine Bricknell <justinegayton@gmail.com>
2018-07-10 14:43:30 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,7 +46,6 @@ class Song
   #look into the array @@all to see if it is already included or not - call self.
   #just using the logic from the two previous methods. just utilze those other two methods you already set up!!!
     if self.find_by_name(name) == true
-      return song.name
        #if self (the class array) find_by_name = the name you pass into it exits  then return that name!
       #if that name does not exist (i.e. you do NOT find it) then you need to create it!
     else self.create_by_name(name) #you must call self. to let the program know WHO/WHAT you are calling this method on.
