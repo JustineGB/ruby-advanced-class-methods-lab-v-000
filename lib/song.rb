fb31e3b073c38f87fabc92a7fd4ec4ae7b6759fd
Justine Bricknell <justinegayton@gmail.com>
2018-07-10 14:36:20 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,18 +3,22 @@ require 'pry'
 #set this class variable to an empty array to store all of those instances
 
 class Song
-  attr_accessor :name, :artist_name
+  attr_accessor :name, :artist_name #making these variables avail to my methods below
   @@all = []
 
   def self.all
+    #this method stores all instances of this class inside of the class variable, the @@all array
     @@all
   end
 
   def save
+    #instnace method.
+    #save each instance and push this into the class.all
     self.class.all << self
   end
 
   def self.create
+    #class method (not just calling on particular instance, method for entire class)???
     song = self.new
     song.save
     song
@@ -38,17 +42,16 @@ class Song
     end
 
   def self.find_or_create_by_name(name)
-    #pass in an argument of the song name
-    #look into the array @@all to see if it is already included or not - call self.
-    if @@all.detect {|song| song.name == name}
-      return song
-    elsif
-      song = self.new
-      song.name = name
-      song.save
-      song
+  #pass in an argument of the song name
+  #look into the array @@all to see if it is already included or not - call self.
+  #just using the logic from the two previous methods. just utilze those other two methods you already set up!!!
+    if self.find_by_name(name) #if self (the class array) find_by_name = the name you pass into it exits  then return that name!
+      return find_by_name(name)
+      #if that name does not exist (i.e. you do NOT find it) then you need to create it!
+    else self.create_by_name(name)
+      return create_by_name(name)
     end
-end
+  end
 
   def self.alphabetical
     @@all.sort
