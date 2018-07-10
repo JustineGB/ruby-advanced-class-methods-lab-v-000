4a5183d3759653a96bf9b81a12525008b5376eb4
Justine Bricknell <justinegayton@gmail.com>
2018-07-10 16:04:05 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -57,7 +57,7 @@ class Song
 
   def self.alphabetical(name)
     #need to create and save these instances by name into the array and then alphabetize!!!!!!
-  @@all.self.sort!
+  @@all.self.sort {|name| return name}
   end
 
   def self.new_from_filename(name, artist_name)
